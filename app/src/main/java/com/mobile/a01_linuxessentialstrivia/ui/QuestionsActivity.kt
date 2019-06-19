ebfa7769fbd16dea3c43b5bbb84fcdf677fb07db
Ofir Bar <ofirbar5@gmail.com>
2019-06-19 03:57:55 +0300

    Moved RadioButtons inside a RadioGroup and added listeners
    The Radio Buttons should listen for the Next button click, as well as click on a specific RadioButton
    The listeners get a reference to the RadioButton that was clicked

--- app/src/main/java/com/mobile/a01_linuxessentialstrivia/ui/QuestionsActivity.kt
+++ app/src/main/java/com/mobile/a01_linuxessentialstrivia/ui/QuestionsActivity.kt
@@ -2,12 +2,38 @@ package com.mobile.a01_linuxessentialstrivia.ui
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import com.mobile.a01_linuxessentialstrivia.R
+import kotlinx.android.synthetic.main.activity_questions.*
+import org.jetbrains.anko.toast
+
 
 class QuestionsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_questions)
+
+        // Get a reference to RadioButton from the RadioGroup
+        activity_questions__question_radio_button_group.setOnCheckedChangeListener(RadioGroup.OnCheckedChangeListener{ group, checkId ->
+            val radio: RadioButton = findViewById(checkId)
+            toast("On ${radio.text}")
+        })
+
+        activity_questions__next_button.setOnClickListener{
+            var id: Int = activity_questions__question_radio_button_group.checkedRadioButtonId
+
+            if (id != -1){
+            val selectedRadio: RadioButton = findViewById(id)
+            toast("On next button click ${selectedRadio.text}")
+            }
+            else{
+                toast(R.string.activity_questions__err_no_question_selected)
+            }
+        }
+
+
+
     }
 }
