4839918cf723b47a4cfe3b5f00d5cd5685321a20
Ofir Bar <ofirbar5@gmail.com>
2019-06-21 08:51:50 +0300

    Added logic to handle user answers
    added logic to check user submitted answers, including wrong and right answers.

--- app/src/main/java/com/mobile/a01_linuxessentialstrivia/data/QuestionsBank.kt
+++ app/src/main/java/com/mobile/a01_linuxessentialstrivia/data/QuestionsBank.kt
@@ -5,18 +5,17 @@ import com.mobile.a01_linuxessentialstrivia.model.Question
 object QuestionsBank{
 
         private var questionsBank = listOf(
-            Question("Which of the following can be used to access the command line?", listOf("Xargs", "XWindows", "BIOS", "Firefox", "Terminal")),
-            Question("Which of the following is a Linux based operating system for use on mobile devices?", listOf("Android","CentOS", "IOS", "Debian")),
-            Question("Where is the BIOS located?", listOf("RAM", "Motherboard", "Hard drive", "LCD Monitor")),
-            Question("What is the number called that is used to identify a process?", listOf("Proc Num", "Process Entry", "PID", "PIN")),
-            Question("Which of the following is a combined audio/video interface for transmission of digital data?", listOf("ATI", "DVI", "VGA", "HDMI", "DVD")),
-            Question("Which of the following commands can be used to create a file?", listOf("Create", "Build", "Nico", "Touch", "Mkfile")),
-            Question("Which of the following commands can be used to extract content from  a tar file?", listOf("Tar -v", "Tar -e", "Tar -c", "Tar -vf", "Tar -xvf")),
-            Question("Which of the following commands can be used to extract files from an archive?", listOf("Tar -e", "Tar -evf", "Tar -vf", "Tar -v", "Tar -xvf")),
-            Question("Which Linux distribution is used as a basis for the creation of Ubuntu Linux?", listOf("Gentoo Linux", "Red Hat Linux", "Arch Linux", "SUSE Linux", "Debian Linux")),
-            Question("Which function does a shell program serve?", listOf("It provides a graphical environment", "It is responsible for logging a user into the system", "It is responsible for logging a user into the system", "It receives user commands and executes them"))
+            Question("Which of the following can be used to access the command line?", listOf("Xargs", "BIOS", "Firefox", "Terminal"), 3),
+            Question("Which of the following is a Linux based operating system for use on mobile devices?", listOf("Android","CentOS", "IOS", "Debian"), 0),
+            Question("Where is the BIOS located?", listOf("RAM", "Hard Drive", "Motherboard", "LCD Monitor"), 2),
+            Question("What is the number called that is used to identify a process?", listOf("Proc Num", "Process Entry", "PID", "PIN"), 2),
+            Question("Which of the following is a combined audio/video interface for transmission of digital data?", listOf("ATI", "DVI", "VGA", "HDMI"), 3),
+            Question("Which of the following commands can be used to create a file?", listOf("Create", "Build", "Nico", "Touch"), 3),
+            Question("Which of the following commands can be used to extract content from  a tar file?", listOf("Tar -v", "Tar -e", "Tar -c", "Tar -xvf"), 3),
+            Question("Which of the following commands can be used to extract files from an archive?", listOf("Tar -e", "Tar -xvf", "Tar -vf", "Tar -v"), 1),
+            Question("Which Linux distribution is used as a basis for the creation of Ubuntu Linux?", listOf("Debian Linux", "Red Hat Linux", "Arch Linux", "SUSE Linux"), 0),
+            Question("Which function does a shell program serve?", listOf("It provides a graphical environment", "It is responsible for logging a user into the system", "It is responsible for logging a user into the system", "It receives user commands and executes them"), 3)
         )
-
 
     fun getInstanceAt(index: Int): Question{
         return questionsBank[index]
--- app/src/main/java/com/mobile/a01_linuxessentialstrivia/ui/QuestionsActivity.kt
+++ app/src/main/java/com/mobile/a01_linuxessentialstrivia/ui/QuestionsActivity.kt
@@ -3,8 +3,8 @@ package com.mobile.a01_linuxessentialstrivia.ui
 import android.annotation.SuppressLint
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.RadioButton
-import android.widget.RadioGroup
 import com.mobile.a01_linuxessentialstrivia.R
 import com.mobile.a01_linuxessentialstrivia.data.QuestionsBank
 import kotlinx.android.synthetic.main.activity_questions.*
@@ -19,67 +19,84 @@ class QuestionsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_questions)
 
-        populateViewsWithData()
-        updateQuestionsProgress()
-        attachListenerOnNextButton()
-
-        // Get a reference to RadioButton from the RadioGroup
-        activity_questions__question_radio_button_group.setOnCheckedChangeListener(RadioGroup.OnCheckedChangeListener{ group, checkId ->
-            val radio: RadioButton = findViewById(checkId)
-            toast("On ${radio.text}")
-        })
-
+        bindViewsWithData() // Initial binding views with data from the QuestionBank Database
+        activity_questions__next_button.setOnClickListener{
+            onAnswerSubmit()
+        }
 
     }
 
-    private fun populateViewsWithData(){
+
+    private fun onAnswerSubmit(){
+        val selectedAnswerViewId: Int = activity_questions__answers_radio_button_group.checkedRadioButtonId
 
         when {
-            currentQuestionIndex < QuestionsBank.getInstance().size -> {
-                val currentQuestion = QuestionsBank.getInstanceAt(currentQuestionIndex)
-                activity_questions__question_title.text = currentQuestion.title
-                activity_questions__question_radio_button_1.text = currentQuestion.answers[0]
-                activity_questions__question_radio_button_2.text = currentQuestion.answers[1]
-                activity_questions__question_radio_button_3.text = currentQuestion.answers[2]
-                activity_questions__question_radio_button_4.text = currentQuestion.answers[3]
+            selectedAnswerViewId != -1 && currentQuestionIndex < QuestionsBank.getInstance().size -> {
+
+                if(isAnswerCorrect(selectedAnswerViewId)){
+                    currentQuestionIndex++
+
+                    // Make sure we don't load a Question index that is outside the scope
+                    if(currentQuestionIndex == QuestionsBank.getInstance().size){
+                        startActivity<FinishActivity>()
+                        finish()
+                    }
+                    else{
+                        clearQuestionsSelection()
+                        bindViewsWithData()
+                    }
+                }
+
             }
-            currentQuestionIndex == QuestionsBank.getInstance().size -> startActivity<FinishActivity>()
+
+            else -> toast(R.string.activity_questions__err_no_answer_selected)
+        }
+    }
+    private fun isAnswerCorrect(selectedAnswerViewId: Int): Boolean{
+        val selectedAnswer: RadioButton = findViewById(selectedAnswerViewId)
+        val selectedAnswerIndex: Int = activity_questions__answers_radio_button_group.indexOfChild(selectedAnswer)
+
+        // selectedAnswerIndex is the answer RadioButton index. we can compare it to the correct answer index from the Database
+        when {
+            selectedAnswerIndex == QuestionsBank.getInstanceAt(currentQuestionIndex).correctAnswerIndex -> {
+                Log.e("currentQuestionIndex==>", currentQuestionIndex.toString())
+                Log.e("correctAnswerIndex==>", QuestionsBank.getInstanceAt(currentQuestionIndex).correctAnswerIndex.toString())
+                toast("Correct!")
+
+                return true
+            }
+            selectedAnswerIndex != QuestionsBank.getInstanceAt(currentQuestionIndex).correctAnswerIndex -> {
+                Log.e("currentQuestionIndex==>", currentQuestionIndex.toString())
+                Log.e("correctAnswerIndex==>", QuestionsBank.getInstanceAt(currentQuestionIndex).correctAnswerIndex.toString())
+                toast("Wrong!")
+                return false
+            }
             else -> {
-                toast("Error")
-                finish()
+                toast("Something got fucked up..")
+                return false
             }
         }
+
+
     }
-
-    private fun attachListenerOnNextButton(){
-        activity_questions__next_button.setOnClickListener{
-            currentQuestionIndex++
-
-            val selectedQuestionId: Int = activity_questions__question_radio_button_group.checkedRadioButtonId
-
-            if (selectedQuestionId != -1){
-                // CheckSolution()
-
-                clearQuestionsSelection()
-                populateViewsWithData()
-                updateQuestionsProgress()
-            }
-            else{
-                toast(R.string.activity_questions__err_no_question_selected)
-            }
-        }
-    }
-
     private fun clearQuestionsSelection(){
-        activity_questions__question_radio_button_1.isChecked = false
-        activity_questions__question_radio_button_2.isChecked = false
-        activity_questions__question_radio_button_3.isChecked = false
-        activity_questions__question_radio_button_4.isChecked = false
+        activity_questions__answer_radio_button_1.isChecked = false
+        activity_questions__answer_radio_button_2.isChecked = false
+        activity_questions__answer_radio_button_3.isChecked = false
+        activity_questions__answer_radio_button_4.isChecked = false
     }
 
-    @SuppressLint("SetTextI18n")
-    private fun updateQuestionsProgress(){
+    private fun bindViewsWithData(){
+        val currentQuestion = QuestionsBank.getInstanceAt(currentQuestionIndex)
+        activity_questions__question_title.text = currentQuestion.title
+        activity_questions__answer_radio_button_1.text = currentQuestion.answers[0]
+        activity_questions__answer_radio_button_2.text = currentQuestion.answers[1]
+        activity_questions__answer_radio_button_3.text = currentQuestion.answers[2]
+        activity_questions__answer_radio_button_4.text = currentQuestion.answers[3]
+
+        @SuppressLint("SetTextI18n")
         activity_questions__progress_bar.text = "${currentQuestionIndex+1}/${QuestionsBank.getInstance().size}"
+
     }
 
 }
