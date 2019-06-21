ad0abd428a843f8e57331dcd983619b0f919568d
Ofir Bar <ofirbar5@gmail.com>
2019-06-21 12:56:24 +0300

    Apply style UI changes to the entire app and add logic to replay the quiz
    The app needs a more "Linux" / "Ubuntu" style. This commit applies it.
    Also, FinishActivity allows users to play the quiz again

--- app/src/main/java/com/mobile/a01_linuxessentialstrivia/ui/FinishActivity.kt
+++ app/src/main/java/com/mobile/a01_linuxessentialstrivia/ui/FinishActivity.kt
@@ -3,11 +3,18 @@ package com.mobile.a01_linuxessentialstrivia.ui
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.mobile.a01_linuxessentialstrivia.R
+import kotlinx.android.synthetic.main.activity_finish.*
+import org.jetbrains.anko.startActivity
 
 class FinishActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_finish)
+
+        activity_finish__start_over.setOnClickListener{
+            startActivity<HomeActivity>()
+            finish()
+        }
     }
 }
